507011a98fa5a59c45ba3bec0776736527099e60
aereal <trasty.loose@gmail.com>
2025-02-27 15:56:44 +0900

    feat: usecases.RegisterRepositorySecret

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/aereal/register-github-secret
 
 go 1.24.0
 
-require github.com/google/go-cmp v0.7.0
+require (
+	github.com/google/go-cmp v0.7.0
+	github.com/google/go-github/v69 v69.2.0
+	golang.org/x/crypto v0.35.0
+)
+
+require (
+	github.com/google/go-querystring v1.1.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+)
---
+++ internal/usecases/register.go
@@ -0,0 +1,72 @@
+package usecases
+
+import (
+	"bytes"
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"log/slog"
+
+	"github.com/google/go-github/v69/github"
+	"golang.org/x/crypto/nacl/box"
+)
+
+func NewRegisterRepositorySecret(client *github.Client) *RegisterRepositorySecret {
+	return &RegisterRepositorySecret{client: client}
+}
+
+type RegisterRepositorySecret struct {
+	client *github.Client
+}
+
+func (u *RegisterRepositorySecret) Do(ctx context.Context, repoOwner string, repoName string, secretName string, plainMsg string) error {
+	pubKey, _, err := u.client.Actions.GetRepoPublicKey(ctx, repoOwner, repoName)
+	if err != nil {
+		return fmt.Errorf("GetRepoPublicKey: %w", err)
+	}
+	serverPubKey, err := getRawPublicKey(pubKey)
+	if err != nil {
+		return err
+	}
+	encrypted, err := encryptAndEncode([]byte(plainMsg), serverPubKey)
+	if err != nil {
+		return err
+	}
+	secret := &github.EncryptedSecret{
+		Name:           secretName,
+		KeyID:          pubKey.GetKeyID(),
+		EncryptedValue: encrypted,
+	}
+	slog.InfoContext(ctx, "set repository secret",
+		slog.String("repo.owner", repoOwner),
+		slog.String("repo.name", repoName),
+		slog.String("secret.name", secretName),
+	)
+	if _, err := u.client.Actions.CreateOrUpdateRepoSecret(ctx, repoOwner, repoName, secret); err != nil {
+		return fmt.Errorf("CreateOrUpdateRepoSecret: %w", err)
+	}
+	return nil
+}
+
+func encryptAndEncode(msg []byte, pubKey *[32]byte) (string, error) {
+	var out []byte
+	got, err := box.SealAnonymous(out, msg, pubKey, rand.Reader)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(got), nil
+}
+
+func getRawPublicKey(pubKey *github.PublicKey) (*[32]byte, error) {
+	rawPubKey, err := base64.StdEncoding.DecodeString(pubKey.GetKey())
+	if err != nil {
+		return nil, fmt.Errorf("base64.Encoding.DecodeString: %w", err)
+	}
+	serverPubKey := new([32]byte)
+	if _, err := io.ReadFull(bytes.NewReader(rawPubKey), serverPubKey[:]); err != nil {
+		return nil, fmt.Errorf("io.ReadFull: %w", err)
+	}
+	return serverPubKey, nil
+}
